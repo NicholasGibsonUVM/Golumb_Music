f45d9ecaed1b124a048055067455fd4f3a346ac4
NicholasGibsonUVM <nsgibson@uvm.edu>
2022-06-07 12:07:14 -0400

    added footer

--- index/foot.php
+++ index/foot.php
@@ -1,1 +1,7 @@
+<footer>
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © Author
+    <a class="text-reset fw-bold">Nicholas Gibson</a>
+  </div>
+</footer>
 </html>
